c122d4fa4f26fab2f50c217ea4c88a90f11b3bf2
gromtoskamp <tom.groskamp@gmail.com>
2020-11-05 16:42:06 +0100

    update registraton

--- registration.php
+++ registration.php
@@ -1,8 +1,6 @@
 <?php
-/**
- * Copyright ©  All rights reserved.
- * See COPYING.txt for license details.
- */
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Groskampweb_Updater', __DIR__);
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Groskampweb_Updater',
+    __DIR__
+);
